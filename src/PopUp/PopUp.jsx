5bc165b43c9c5f18ac7168c13c135d9068c89030
Mateo <mateobinance1@gmail.com>
2025-05-26 16:05:50 -0300

    cambios para vercel

--- src/PopUp/PopUp.jsx
+++ src/PopUp/PopUp.jsx
@@ -1,5 +1,5 @@
-import './style/popup.css'
 
+import './style/popup.css'
 import { useNavegacion } from '../hooks/useNavegacion'
 import { useState,useEffect } from 'react'
 export const PopUp = ({content,role,event,ref,namebtn,error,nombre,isSuccess}) => {
