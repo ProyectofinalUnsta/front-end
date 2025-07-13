a904d3015d14aec527b77539555576193bc820db
Juan Manuel Gimenez <juanmauel20@gmail.com>
2025-07-13 00:26:24 -0300

    Update endpoints.js

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,33 +1,35 @@
-const endpoints = {}
+const endpoints = {};
 
-if(import.meta.env.MODE ===   'production'){
-    endpoints.get = import.meta.env.VITE_ENDPOINT_GET,
-    endpoints.keepserveralive = import.meta.env.VITE_ENDPOINT_SERVER,
-    endpoints.keepservicealive = import.meta.env.VITE_SERVICE_IMAGE,
-    endpoints.keepmailalive = import.meta.env.VITE_SERVICE_MAIL,
-    endpoints.post = import.meta.env.VITE_ENDPOINT_POST,
-    endpoints.login = import.meta.env.VITE_ENDPOINT_LOGIN,
-    endpoints.register = import.meta.env.VITE_ENDPOINT_REGISTER,
-    endpoints.logout = import.meta.env.VITE_ENDPOINT_LOGOUT
-    endpoints.eventoPorId = import.meta.env.VITE_ENDPOINT_EVENTOID
-    endpoints.files = import.meta.env.VITE_ENDPOINT_FILES
-    endpoints.presentaciones = import.meta.env.VITE_ENDPOINT_PRESENTACIONES
-    endpoints.inscripciones = import.meta.env.VITE_ENDPOINT_INSCRIPCIONES
-    endpoints.disertantes = import.meta.env.VITE_ENDPOINT_DISERTANTES
+if (import.meta.env.MODE === "production") {
+  (endpoints.get = import.meta.env.VITE_ENDPOINT_GET),
+    (endpoints.keepserveralive = import.meta.env.VITE_ENDPOINT_SERVER),
+    (endpoints.keepservicealive = import.meta.env.VITE_SERVICE_IMAGE),
+    (endpoints.keepmailalive = import.meta.env.VITE_SERVICE_MAIL),
+    (endpoints.post = import.meta.env.VITE_ENDPOINT_POST),
+    (endpoints.login = import.meta.env.VITE_ENDPOINT_LOGIN),
+    (endpoints.register = import.meta.env.VITE_ENDPOINT_REGISTER),
+    (endpoints.logout = import.meta.env.VITE_ENDPOINT_LOGOUT);
+  endpoints.eventoPorId = import.meta.env.VITE_ENDPOINT_EVENTOID;
+  endpoints.files = import.meta.env.VITE_ENDPOINT_FILES;
+  endpoints.presentaciones = import.meta.env.VITE_ENDPOINT_PRESENTACIONES;
+  endpoints.inscripciones = import.meta.env.VITE_ENDPOINT_INSCRIPCIONES;
 }
-if(import.meta.env.MODE === 'development'){
-    endpoints.get = 'http://localhost:3001/api/',
-    endpoints.keepserveralive = 'http://localhost:3001',
-    endpoints.keepservicealive = 'http://localhost:3001',
-    endpoints.keepmailalive = 'http://localhost:3001',
-    endpoints.post = 'http://localhost:3001/api/admin/',
-    endpoints.login = 'http://localhost:3001/api/auth/login',
-    endpoints.register = 'http://localhost:3001/api/auth/register',
-    endpoints.logout = 'http://localhost:3001/api/auth/logout',
-    endpoints.eventoPorId = 'http://localhost:3001/api/admin/',
-    endpoints.files = 'http://localhost:3001/api/files',
-    endpoints.presentaciones = 'http://localhost:3001/api/presentations/'
-    endpoints.inscripciones = 'http://localhost:3001/api/inscriptos/'
-    endpoints.disertantes = 'http://localhost:3001/api/disertante/'
+if (import.meta.env.MODE === "development") {
+  (endpoints.get = "https://back-end-fiq8.onrender.com/api/"),
+    (endpoints.keepserveralive = "https://back-end-fiq8.onrender.com"),
+    (endpoints.keepservicealive =
+      "https://convertidor-webp-service.onrender.com"),
+    (endpoints.keepmailalive = "https://email-sender-qs8y.onrender.com"),
+    (endpoints.post = "https://back-end-fiq8.onrender.com/api/admin/"),
+    (endpoints.login = "https://back-end-fiq8.onrender.com/api/auth/login"),
+    (endpoints.register =
+      "https://back-end-fiq8.onrender.com/api/auth/register"),
+    (endpoints.logout = "https://back-end-fiq8.onrender.com/api/auth/logout"),
+    (endpoints.eventoPorId = "https://back-end-fiq8.onrender.com/api/admin/"),
+    (endpoints.files = "https://back-end-fiq8.onrender.com/api/files"),
+    (endpoints.presentaciones =
+      "https://back-end-fiq8.onrender.com/api/presentations/");
+  endpoints.inscripciones =
+    "https://back-end-fiq8.onrender.com/api/inscriptos/";
 }
-export default endpoints
+export default endpoints;
