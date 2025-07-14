e75c701e99e0c3ee123b9e0f830c9765eaaa5c9d
Juan Manuel Gimenez <juanmauel20@gmail.com>
2025-07-14 03:41:15 -0300

    nav 404r

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -6,9 +6,13 @@ export default function ErrorPage() {
   return (
     <div className="error404-container">
       {/* Usamos la imagen importada y le aplicamos una clase para el estilo y la animación */}
-      <img src={missileImage} alt="Misil girando" className="spinning-missile" />
       
-      <h1 className="font-404-text">Error 404</h1>
+      
+      <h1 className="font-404-text"> Error 
+        <span>4</span> 
+        <img src={missileImage} alt="Misil girando" className="spinning-missile" />
+        <span>4</span>
+      </h1> 
       <h1 className="descripcion-text">¡Oops! Página no encontrada</h1>
       <p className="small-text">Lo sentimos, pero la página que buscas no existe.</p>
       
