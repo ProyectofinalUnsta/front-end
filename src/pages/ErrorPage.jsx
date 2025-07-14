4373d8fceb7f22574501b21a0003389a969d4904
Juan Manuel Gimenez <juanmauel20@gmail.com>
2025-07-14 02:40:46 -0300

    img error 404

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
-import "../global/Error404.css"; // Asegúrate de que existan estilos en tu CSS
+import "../global/Error404.css"; // Asegúrate de que este archivo CSS exista
+import missileImage from '../assets/missile-impact.png'; // Importa tu imagen del misil
 
-export default function ErrorPage  ()  {
+export default function ErrorPage() {
   return (
     <div className="error404-container">
-      {/* <img src="/assets/loading-logo.png" alt="Logo de carga" className="loading-logo" /> */}
-      <h1 className="font-404-text">404</h1>
+      {/* Usamos la imagen importada y le aplicamos una clase para el estilo y la animación */}
+      <img src={missileImage} alt="Misil girando" className="spinning-missile" />
+      
+      <h1 className="font-404-text">Error 404</h1>
       <h1 className="descripcion-text">¡Oops! Página no encontrada</h1>
       <p className="small-text">Lo sentimos, pero la página que buscas no existe.</p>
+      
       <Link to="/" className="home-link">
         Volver al inicio
       </Link>
     </div>
   );
-};
-
+}
